12cbed286491b513332a769f36f147de7b420844
abhishek <abhi445268@gmai.com>
2025-08-30 21:42:47 +0530

    pdf merger code

--- pdfmerger.py
+++ pdfmerger.py
@@ -1,2 +1,11 @@
 #write an code which take 3or more pdf and merge them together into single pdf
-import pypdf
+from pypdf import PdfWriter
+
+merger = PdfWriter()
+
+for pdf in ["pdf/data1.pdf","pdf/balise ppt data.pdf","pdf/getting-started-with-pico.pdf"]:#pdf names enter here
+
+    merger.append(pdf)
+
+merger.write("merged-pdf.pdf")#merged pdf is of name merged-pdf.pdf
+merger.close()
