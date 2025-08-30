527620f4319e7cfd8e89534464d98d9b82ebcec1
abhishek <abhi445268@gmai.com>
2025-08-30 22:32:14 +0530

    shoutout.py is deleted

--- shoutout.py
+++
@@ -1,2 +0,0 @@
-#here we use a library which will take words from us and convert those words into the text format
-# this is helpful when we are not able to communicate with any other person which is deaf
