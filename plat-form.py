024c5aebccb61068500fcb3e7b79d2691bb0bd95
abhishek <abhi445268@gmai.com>
2025-08-31 11:36:05 +0530

    hey i am adding code through the python module

---
+++ plat-form.py
@@ -0,0 +1,32 @@
+import platform
+# platfrom module help us to know our system
+import os
+
+print(platform.system())# in which system we are working on
+
+print(platform.release())# which 6.12.41+deb13-amd64
+
+print(platform.machine())# x-86_64
+
+print(platform.processor())
+
+print(platform.python_version())# python version will print out
+
+print(platform.platform())# give platform data
+
+try:
+    if platform.system() =="Window":
+        print("hey you are working in window")
+
+    elif platform.system()=="Linux":
+        print("hey you are working in linux")
+    else:
+        print("please mention your system in code")
+    os.system("git status")
+    os.system("git add . ")
+    comment = input("please enter comment for repo :  ")
+    os.system(f"git commit -m'{comment}'  ")
+    os.system("git push origin main")
+    
+except:
+    print('some error occured')
