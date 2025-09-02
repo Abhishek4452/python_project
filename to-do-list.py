76d251326f736dd8d2cdb3991e2d15668bae9fab
abhishek <abhi445268@gmai.com>
2025-09-02 12:07:47 +0530

    adding more file

---
+++ to-do-list.py
@@ -0,0 +1,6 @@
+# To-Do List App (Console Based)
+
+# Add, delete, and mark tasks as complete.
+
+# Save tasks to a text/JSON file so they remain after restarting the program.
+# # (Concepts: File handling, lists, dictionaries)
---
+++ whether-monitoring.py
@@ -0,0 +1,9 @@
+# Weather App (using API)
+
+# Take a city name as input.
+
+# Fetch real-time weather using [OpenWeatherMap API].
+
+# Show temperature, humidity, and description.
+# # (Concepts: requests, JSON, APIs)
+
