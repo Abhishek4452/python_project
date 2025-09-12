60c443464e788a2cdb61689a4fe1def54988230e
abhishek <abhi445268@gmai.com>
2025-09-12 22:34:17 +0530

    making a new folder for the to do list application

--- to-do-list.py
+++
@@ -1,6 +0,0 @@
-# To-Do List App (Console Based)
-
-# Add, delete, and mark tasks as complete.
-
-# Save tasks to a text/JSON file so they remain after restarting the program.
-# # (Concepts: File handling, lists, dictionaries)
