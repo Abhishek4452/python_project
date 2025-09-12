77d0c8f398d254c1bdb0e46fb651eeb6c8e8081d
abhishek <abhi445268@gmai.com>
2025-09-12 21:32:08 +0530

    modifing the text inside the os module

--- os-module.py
+++ os-module.py
@@ -1,1 +1,9 @@
 #here i will learn the basic os module and its working
+import os
+if not os.path.exists("data"):
+    os.mkdir("data")
+
+for i in range(1,100):
+    #os.rmdir(f"data\day{i}")       # for removing the directory 
+    os.mkdir(f"data/day{i}")       #for making the file
+    
